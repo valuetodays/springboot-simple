6d61cd17d7cbfcdcbd628adc06dce9084f453d5d
lei.liu <lei.liu>
2022-04-12 20:09:28 +0800

    init

---
+++ src/main/java/com/simple/App.java
@@ -0,0 +1,18 @@
+package com.simple;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * .
+ *
+ * @author lei.liu
+ * @since 2020-07-15 14:49
+ */
+@SpringBootApplication
+public class App {
+    public static void main(String[] args) {
+        SpringApplication.run(App.class, args);
+    }
+}
+
---
+++ src/main/java/com/simple/IndexController.java
@@ -0,0 +1,23 @@
+package com.simple;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * .
+ *
+ * @author lei.liu
+ * @since 2020-07-15 14:50
+ */
+@RestController
+public class IndexController {
+
+    @GetMapping("/")
+    public String hostName() throws UnknownHostException {
+        return InetAddress.getLocalHost().getHostName();
+    }
+}
+
